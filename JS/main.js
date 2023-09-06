5ad351c56274665fb42465424033411f9018ec7f
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-09-06 16:03:44 +0200

    feat print 10 email random

--- JS/main.js
+++ JS/main.js
@@ -4,9 +4,29 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello World'
+            emailCasuali: [''],
+            emailActive: 0,
+        };
+
+    },
+
+    mounted() {
+
+
+        for (let i = 0; i < 10; i++) {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    const randomEmail = response.data.response;
+
+                    this.emailCasuali.push({
+                        emailCasuali: randomEmail,
+                    })
+                    console.log(this.emailCasuali);
+                });
+
         }
-    }
+    },
 }).mount('#app')
 
 
